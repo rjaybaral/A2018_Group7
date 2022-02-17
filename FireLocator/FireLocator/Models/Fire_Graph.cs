184ae402576082779936b4797a81948aed663b98
rjaybaral <74280705+rjaybaral@users.noreply.github.com>
2022-02-18 01:47:55 +0800

    FireLocator

---
+++ FireLocator/FireLocator/Models/Fire_Graph.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace FireLocator.Models
+{
+    public class Fire_Graph
+    {
+
+        [XmlElement("0")]
+        public string Heat_Level { get; set; }
+        [XmlElement("1")]
+        public string Date_Time { get; set; }
+    }
+}
